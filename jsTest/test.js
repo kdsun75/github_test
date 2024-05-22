f3c1575368ab72df49be17583ce79c28dc216879
kdsun75 <kdsun75@gmail.com>
2024-05-22 18:35:01 +0900

    [update]js study-git

---
+++ jsTest/test.js
@@ -0,0 +1,12 @@
+
+const seatingChart = [
+    ['Kristen', 'Erik', 'Namita'],
+    ['Geoffrey', 'Juanita', 'Antonio', 'Kevin'],
+    ['Yuma', 'Sakura', 'Jack', 'Erika']
+]
+
+for(let i=0; i<seatingChart.length; i++){
+    for(let j=0; j<seatingChart[i].length; j++)
+        console.log(`${seatingChart[i][j]} `)
+}
+
