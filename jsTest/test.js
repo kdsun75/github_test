e77618c2754244a6d9545bdfea88ee82377f8f28
kdsun75 <kdsun75@gmail.com>
2024-05-23 17:44:51 +0900

    [uddate]study-24.05.23

--- jsTest/test.js
+++ jsTest/test.js
@@ -1,4 +1,5 @@
 
+/*
 const seatingChart = [
     ['Kristen', 'Erik', 'Namita'],
     ['Geoffrey', 'Juanita', 'Antonio', 'Kevin'],
@@ -9,4 +10,23 @@ for(let i=0; i<seatingChart.length; i++){
     for(let j=0; j<seatingChart[i].length; j++)
         console.log(`${seatingChart[i][j]} `)
 }
+*/
+/*
+let input = prompt("Hey, say something!");
+while(true) {
+    input = prompt(input);
+    if(input.toLocaleLowerCase() === "stop copying me") break;
+}
+console.log("OK YOU WIN");
+*/
 
+let maxinum = parseInt(prompt("Enter the maxnumber: "));
+console.log(maxinum);
+/*
+while(!maxinum){
+    maxinum = parseInt(prompt("Enter the valid number: "));
+}
+
+const targetNum = Math.floor(Math.random() * maxinum) + 1;
+console.log(targetNum);
+*/
