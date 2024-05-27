0d004d9f9602e44d9e35ce4322ea52828bca82ed
kdsun75 <kdsun75@gmail.com>
2024-05-27 18:38:52 +0900

    no message

--- jsTest/Todo.js
+++ jsTest/Todo.js
@@ -1,5 +1,16 @@
-let message = prompt('The page says');
+let input = prompt('What would you like to do?');
 
-while(message !== 'quit'){
-    message = prompt('The page says');
+const todos = ['Collection Chicken Eggs', 'Clean Litter Box'];
+
+while(input !== 'quit' && input !=='q'){
+    if(input === 'list'){
+        console.log('*************************');
+        for(let i=0; i<todos.length; i++){
+            console.log(`${i}: ${todos[i]}`);
+        }
+        console.log('*************************');
+    }
+    input = prompt('What would you like to do?');
 }
+
+console.log('OK QUIT THE APP!');
