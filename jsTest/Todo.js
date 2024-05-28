5cf6528b8d3e101683b1857ffbc0f54c01115c42
kdsun75 <kdsun75@gmail.com>
2024-05-28 11:57:31 +0900

    [update]Todolist exam(easy ver.)

--- jsTest/Todo.js
+++ jsTest/Todo.js
@@ -9,8 +9,24 @@ while(input !== 'quit' && input !=='q'){
             console.log(`${i}: ${todos[i]}`);
         }
         console.log('*************************');
+    }else if(input === 'new'){
+        const newTodo = prompt("OK, what is the new todo?");
+        todos.push(newTodo);
+    }else if(input === 'delete'){
+        const index = parseInt(prompt('OK, enter an index to delete:'));
+        if(!Number.isNaN(index)){ //숫자면     
+            const deleted = todos.splice(index, 1);  // 반환값은 삭제된 요소들을 반환(여러개일수 있음)
+            console.log(`OK. deleted: ${deleted[0]}`);
+        }else{
+            console.log("Unknown index!");
+        }
+        
+        
+    }else if(input === 'modify'){
+
     }
+
     input = prompt('What would you like to do?');
 }
 
-console.log('OK QUIT THE APP!');
+console.log('OK QUIT THE APP!'); 
