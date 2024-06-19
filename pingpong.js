1ba4d049ca8b3c21e95e24256c50c0710dfedcc6
kdsun <kdsun75@gmail.com>
2024-06-19 23:59:21 +0900

    6/19-v3

--- pingpong.js
+++ pingpong.js
@@ -16,8 +16,11 @@ p1Button.addEventListener('click', function(){
         p1Score +=1;
         if(p1Score === winningScore){
             isGameOver = true;
-            p1Display.classList.add('winner');
-            p2Display.classList.add('loser');
+            p1Display.classList.add('has-text-success');
+            p2Display.classList.add('has-text-danger');
+            p1Button.disabled = true;
+            p2Button.disabled = true;
+
         }
         p1Display.textContent = p1Score;
     }
@@ -28,8 +31,10 @@ p2Button.addEventListener('click', function(){
         p2Score +=1;
         if(p2Score === winningScore){
             isGameOver = true;
-            p2Display.classList.add('winner');
-            p1Display.classList.add('loser');
+            p2Display.classList.add('has-text-success');
+            p1Display.classList.add('has-text-danger');
+            p1Button.disabled = true;
+            p2Button.disabled = true;
         }
         p2Display.textContent = p2Score;
     }
@@ -42,8 +47,10 @@ function reset(){
   //  p1Display.style.color = 'cadetblue';
     p2Display.textContent = 0;
   //  p2Display.style.color = 'cadetblue';
-    p1Display.classList.remove('winner', 'loser');
-    p2Display.classList.remove('winner', 'loser');
+    p1Display.classList.remove('has-text-success', 'has-text-danger');
+    p2Display.classList.remove('has-text-success', 'has-text-danger');
+    p1Button.disabled = false;
+    p2Button.disabled = false;
 
 }
 
